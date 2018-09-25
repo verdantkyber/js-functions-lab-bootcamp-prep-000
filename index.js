c348818498757d8ed34213e25f48606b1721d887
verdantkyber <kingjalexa@gmail.com>
2018-09-25 02:44:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // write your code below!
 function happyHolidays(){
-  return "Happy holidays!";
+  return ("Happy holidays!");
 }
 
 function happyHolidaysTo(name){
-  return "Happy Holidays,"; name "!"
+  return ("Happy Holidays,"+ name + "!");
 }
